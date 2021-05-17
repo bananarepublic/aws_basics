943f1d5782b5b0a89deab9c2c7119dba67353b58
Tatsiana Klionskaya <tatsiana_klionskaya@epam.com>
2021-05-17 16:30:38 +0000

    Rewrite stack in yaml

--- 4_ec2/create_stack.sh
+++ 4_ec2/create_stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name TK-EC2-Stack --template-body file://tk_ec2_stack.cf --profile admin
+aws cloudformation create-stack --stack-name TK-EC2-Stack --template-body file://tk_ec2_stack.yaml --profile admin
--- 4_ec2/update_stack.sh
+++ 4_ec2/update_stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation update-stack --stack-name TK-EC2-Stack --template-body file://tk_ec2_stack.cf --profile admin
+aws cloudformation update-stack --stack-name TK-EC2-Stack --template-body file://tk_ec2_stack.yaml --profile admin

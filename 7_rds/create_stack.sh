764154de102b475fc7c86a57c3ad65ed8b8b2e51
Tatsiana Klionskaya <tatsiana_klionskaya@epam.com>
2021-05-18 16:35:14 +0000

    Add S3 bucket to stack

--- 7_rds/create_stack.sh
+++ 7_rds/create_stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name TK-RDS-Stack --template-body file://tk_rds_stack.yaml --profile admin
+aws cloudformation create-stack --stack-name TK-RDS-Stack --template-body file://tk_rds_stack.yaml --capabilities CAPABILITY_IAM --profile admin
--- 7_rds/update_stack.sh
+++ 7_rds/update_stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation update-stack --stack-name TK-RDS-Stack --template-body file://tk_rds_stack.yaml --profile admin
+aws cloudformation update-stack --stack-name TK-RDS-Stack --template-body file://tk_rds_stack.yaml --capabilities CAPABILITY_IAM --profile admin

065b32a5b5ec3600510477b7bf4a15b9a6966b06
Tatsiana Klionskaya <tatsiana_klionskaya@epam.com>
2021-05-22 17:57:50 +0000

    Add RDS to stack

--- 7_rds/update_stack.sh
+++ 7_rds/update_stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation update-stack --stack-name TK-RDS-Stack --template-body file://tk_rds_stack.yaml --capabilities CAPABILITY_IAM --profile admin
+aws cloudformation update-stack --stack-name TK-RDS-Stack --template-body file://tk_rds_stack.yaml --parameters file://params.json --capabilities CAPABILITY_IAM --profile admin

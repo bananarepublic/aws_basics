2404961c4edcc0f9f6646e6ac8f684676de48702
Tatsiana Klionskaya <tatsiana_klionskaya@epam.com>
2021-06-05 13:16:41 +0000

    Add Lambda to stack

--- 9_lambda/server.py
+++ 9_lambda/server.py
@@ -192,33 +192,6 @@ class SubsHandler(RequestHandler):
             self.set_status(500)
 
 
-def send_sqs_messages_to_sns():
-    sns = boto3.client('sns', region_name=get_region())
-    sqs = boto3.client('sqs', region_name=get_region())
-
-    while True:
-        response = sqs.receive_message(
-            QueueUrl=SQS_QUEUE,
-            WaitTimeSeconds=20,
-        )
-
-        if 'Messages' not in response:
-            print('No messages')
-            continue
-
-        for message in response['Messages']:
-            message_body = message['Body']
-            response = sns.publish(
-                TargetArn=SNS_TOPIC,
-                Message=message_body,
-                Subject='New Event',
-            )
-            print('Send message to SNS')
-            receipt_handle = message['ReceiptHandle']
-            sqs.delete_message(QueueUrl=SQS_QUEUE, ReceiptHandle=receipt_handle)
-            print('Delete message from SQS')
-
-
 def make_app():
     return Application([
         (r'/', MainHandler),
